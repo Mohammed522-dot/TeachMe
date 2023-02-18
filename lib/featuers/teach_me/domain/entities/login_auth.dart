886dccc3fd58684d91ec78b84339f114056b79b1
moahamed88 <mohamedibrahim132@yahoo.com>
2023-02-18 12:24:23 +0400

    yaml update

--- lib/featuers/teach_me/domain/entities/login_auth.dart
+++ lib/featuers/teach_me/domain/entities/login_auth.dart
@@ -1,8 +1,10 @@
 import 'package:equatable/equatable.dart';
-
-class AuthLogin extends Equatable{
-    final String email;
-    final String password;
-
-    AuthLogin({required this.email,required this.password}): super([email,password]);
-}
+//
+// class AuthLogin extends Equatable{
+//     final String email;
+//     final String password;
+//
+//     AuthLogin({required this.email,required this.password}): super([email,password]);
+//
+//
+// }
--- lib/featuers/teach_me/domain/repositories/auth_login_repository.dart
+++ lib/featuers/teach_me/domain/repositories/auth_login_repository.dart
@@ -2,6 +2,6 @@ import 'package:dartz/dartz.dart';
 import 'package:teachme/core/error/failuers.dart';
 import 'package:teachme/featuers/teach_me/domain/entities/login_auth.dart';
 
-abstract class AuthLoginRepository {
- Future<Either<Failure,AuthLogin>>getAuthLogin();
-}
+// abstract class AuthLoginRepository {
+//  Future<Either<Failure,AuthLogin>>getAuthLogin();
+// }
